cb873166f4faaae9ceca6a8df97f42e34a2546e0
fenixblanco18 <montero.helena9@gmail.com>
2024-03-20 19:53:39 +0100

    uuu

--- Assets/GameAsset/Scripts/Explosion.cs
+++ Assets/GameAsset/Scripts/Explosion.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
+    public GameObject prefabExplosion;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  public void OnCollisionEnter();
+     Instantiate(prefabExplosion, transform.position, transform.rotation);
+     Destroy(GameObject);
 }
